ea13c17d196561d2ead8e473a67195426ffe2131
Blake Exe <36413296-fnafexe619@users.noreply.replit.com>
2026-05-24 03:14:54 +0000

    Fix Hermes Babel transpilation

--- babel.config.js
+++ babel.config.js
@@ -7,7 +7,6 @@ module.exports = function(api) {
       '@babel/plugin-transform-class-properties',
       '@babel/plugin-transform-private-methods',
       '@babel/plugin-transform-private-property-in-object',
-      'react-native-reanimated/plugin',
     ],
   };
 };
